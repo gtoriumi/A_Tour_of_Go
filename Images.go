6fedfaa6daf38ad35508273def1241c616ffea78
Go Toriumi <gtoriumi@gmail.com>
2017-01-08 21:20:18 +0900

    Add the answer of "Images"
    I add the answer of "Exercise: Images".

---
+++ Images.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"golang.org/x/tour/pic"
+	"image/color"
+	"image"
+)
+
+type Image struct{}
+
+func (img Image) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+func (img Image) Bounds() image.Rectangle {
+	return image.Rect(0, 0, 255, 255)
+}
+
+func (img Image) At(x, y int) color.Color {
+	return color.RGBA{uint8(x^y), uint8(x^y), 255, 255}
+}
+
+func main() {
+	m := Image{}
+	pic.ShowImage(m)
+}
